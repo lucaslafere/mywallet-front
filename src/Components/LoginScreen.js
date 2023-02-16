0ddfc7ce4ac282628d3627a5286b9fc6bea32987
Lucas Lafèré <laferelucas@gmail.com>
2023-02-16 16:21:08 -0300

    added dotenv example

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -1,117 +1,136 @@
-import { useState, useContext, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import styled from 'styled-components';
-import { ThreeDots } from 'react-loader-spinner';
-import TokenContext from '../Contexts/TokenContext';
+import axios from "axios";
+import { useContext, useState } from "react";
+import { ThreeDots } from "react-loader-spinner";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import TokenContext from "../Contexts/TokenContext";
 
 export default function LoginScreen() {
-    const [disabled, setDisabled] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [errorText, setErrorText] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const URL = "https://my-wallet-api.netlify.app/login";
-    const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const deployUrl = "https://my-wallet-api.netlify.app/login";
+  const URL = "http://localhost:4000/login";
+  const navigate = useNavigate();
 
-    const { setToken, token } = useContext(TokenContext);
+  const { setToken } = useContext(TokenContext);
 
-    const body = {
-        email,
-        password
+  const body = {
+    email,
+    password,
+  };
+
+  function login(event) {
+    event.preventDefault();
+    setDisabled(true);
+    setLoading(true);
+    requestLogin();
+  }
+
+  function requestLogin() {
+    axios
+      .post(URL, body)
+      .then((res) => {
+        setToken(res.data.token);
+        setLoading(false);
+        navigate("/receipts");
+      })
+      .catch(() => {
+        setDisabled(false);
+        setLoading(false);
+        setErrorText(
+          "não foi possível logar. Verifique seus dados e tente novamente, ou crie uma conta nova"
+        );
+        setError(true);
+      });
+  }
+
+  function openModal() {
+    if (error) {
+      return (
+        <ContainerModal onClick={() => setError(false)}>
+          <h5>Houve um erro: {errorText}</h5>
+          <h5>Clique em qualquer lugar da caixa para retornar</h5>
+        </ContainerModal>
+      );
     }
+  }
 
-    function login(event) {
-        event.preventDefault();
-        setDisabled(true);
-        setLoading(true);
-        requestLogin();
+  const openError = openModal();
 
-    }
-
-
-    function requestLogin() {
-        axios.post(URL, body)
-            .then((res) => {
-                setToken(res.data.token);
-                setLoading(false);
-                navigate("/receipts");
-            })
-            .catch(() => {
-                setDisabled(false);
-                setLoading(false);
-                setErrorText("não foi possível logar. Verifique seus dados e tente novamente, ou crie uma conta nova")
-                setError(true);
-            })
-    }
-
-    function openModal() {
-        if (error) {
-            return (
-                <ContainerModal onClick={() => setError(false)}>
-                    <h5>Houve um erro: {errorText}</h5>
-                    <h5>Clique em qualquer lugar da caixa para retornar</h5>
-                </ContainerModal>
-            )
-        }
-    }
-
-    const openError = openModal();
-
-    return (
-        <>
-            {error ? openError : null}
-            <Container error={error}>
-                <MyWallet>
-                    <h1>MyWallet</h1>
-                </MyWallet>
-                <Form onSubmit={login} >
-                    <Input
-                        placeholder="E-mail" type="email" autoComplete="email" disabled={disabled} value={email} onChange={e => setEmail(e.target.value)} />
-                    <Input
-                        placeholder="Senha" type="password" autoComplete="current-password" disabled={disabled} value={password} onChange={e => setPassword(e.target.value)} />
-                    <Button
-                        disabled={disabled}
-                        type="submit" >
-                        {loading ? <ThreeDots color="#fff" height={80} width={80} /> : "Entrar"}
-                    </Button>
-                </Form>
-                <Link to={"/sign-up"}>
-                    <TextLink>Primeira vez? Cadastre-se!</TextLink>
-                </Link>
-            </Container>
-
-
-        </>
-    )
+  return (
+    <>
+      {error ? openError : null}
+      <Container error={error}>
+        <MyWallet>
+          <h1>MyWallet</h1>
+        </MyWallet>
+        <Form onSubmit={login}>
+          <Input
+            placeholder='E-mail'
+            type='email'
+            autoComplete='email'
+            disabled={disabled}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder='Senha'
+            type='password'
+            autoComplete='current-password'
+            disabled={disabled}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            disabled={disabled}
+            type='submit'>
+            {loading ? (
+              <ThreeDots
+                color='#fff'
+                height={80}
+                width={80}
+              />
+            ) : (
+              "Entrar"
+            )}
+          </Button>
+        </Form>
+        <Link to={"/sign-up"}>
+          <TextLink>Primeira vez? Cadastre-se!</TextLink>
+        </Link>
+      </Container>
+    </>
+  );
 }
 
-
 const ContainerModal = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-width: 90%;
-height: 50%;
-background-color: #A329D6;
-border-radius: 12px;
-z-index: 1;
-position: fixed;
-top: 25vh;
-left: 5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  height: 50%;
+  background-color: #a329d6;
+  border-radius: 12px;
+  z-index: 1;
+  position: fixed;
+  top: 25vh;
+  left: 5vw;
 
-h5{
-    font-family: 'Raleway', sans-serif;
+  h5 {
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 1.2rem;
     color: #fff;
     text-align: center;
-}
-h6 {
-    font-family: 'Raleway', sans-serif;
+  }
+  h6 {
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 2rem;
@@ -119,79 +138,78 @@ h6 {
     text-align: center;
     margin-top: 10px;
     margin-bottom: 10px;
-}
-`
+  }
+`;
 
 const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-height: 100vh;
-background-color: #8C12BE;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: #8c12be;
+`;
 const MyWallet = styled.div`
-margin-bottom: 2rem;
-font-family: 'Saira Stencil One', cursive;
-font-style: Regular;
-font-size: 32px;
-color: #FFF;
-`
+  margin-bottom: 2rem;
+  font-family: "Saira Stencil One", cursive;
+  font-style: Regular;
+  font-size: 32px;
+  color: #fff;
+`;
 const Form = styled.form`
-display: flex;
-flex-direction: column;
-align-items: center;
-width: 100%;
-gap: 0.8rem;
-margin-bottom: 2rem;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 0.8rem;
+  margin-bottom: 2rem;
+`;
 const Input = styled.input`
-display: flex;
-align-items: center;
-width: 90%;
-height: 58px;
-border-radius: 6px;
-border: ${props => props.disabled ? "1px solid #d69bef" : "1px solid #FFF"};
-padding: 1rem;
-opacity: ${props => props.disabled ? 0.7 : 1};
-background-color: ${props => props.disabled ? '#d69bef' : '#fff'};
-font-family: 'Raleway', sans-serif;
-font-style: normal;
-font-weight: 500;
-font-size: 1.2rem;
-color: #000000;
-::placeholder{
-font-size: 1.2rem;
-color: ${props => props.disabled ? '#afafaf' : '#000000'};
-font-family: 'Raleway', sans-serif;
-font-style: normal;
-font-weight: 500;
-}
-
-
-`
+  display: flex;
+  align-items: center;
+  width: 90%;
+  height: 58px;
+  border-radius: 6px;
+  border: ${(props) =>
+    props.disabled ? "1px solid #d69bef" : "1px solid #FFF"};
+  padding: 1rem;
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+  background-color: ${(props) => (props.disabled ? "#d69bef" : "#fff")};
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.2rem;
+  color: #000000;
+  ::placeholder {
+    font-size: 1.2rem;
+    color: ${(props) => (props.disabled ? "#afafaf" : "#000000")};
+    font-family: "Raleway", sans-serif;
+    font-style: normal;
+    font-weight: 500;
+  }
+`;
 const Button = styled.button`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 90%;
-height: 50px;
-border-radius: 6px;
-border: 1px solid #A329D6;
-background-color: #A329D6;
-padding: 1rem;
-opacity: ${props => props.disabled ? 0.7 : 1};
-font-family: 'Raleway', sans-serif;
-font-style: normal;
-font-weight: 700;
-font-size: 1.2rem;
-color: #FFFFFF;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  height: 50px;
+  border-radius: 6px;
+  border: 1px solid #a329d6;
+  background-color: #a329d6;
+  padding: 1rem;
+  opacity: ${(props) => (props.disabled ? 0.7 : 1)};
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.2rem;
+  color: #ffffff;
+`;
 const TextLink = styled.div`
-font-family: 'Raleway', sans-serif;
-font-style: normal;
-font-weight: 700;
-font-size: 1rem;
-color: #FFFFFF;
-`
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1rem;
+  color: #ffffff;
+`;
 export { Container, MyWallet, Form, Input, Button, TextLink, ContainerModal };
--- src/Components/SignUpScreen.js
+++ src/Components/SignUpScreen.js
@@ -1,130 +1,167 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from 'react-router-dom';
-import { ThreeDots } from 'react-loader-spinner';
-import { Container, MyWallet, Form, Input, Button, TextLink, ContainerModal } from "./LoginScreen";
+import { Link, useNavigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
+import {
+  Container,
+  MyWallet,
+  Form,
+  Input,
+  Button,
+  TextLink,
+  ContainerModal,
+} from "./LoginScreen";
 
 export default function SignUpScreen() {
-    const [disabled, setDisabled] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [errorText, setErrorText] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [name, setName] = useState("");
-    const URL = "https://my-wallet-api.netlify.app/sign-up";
-    const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const deployUrl = "https://my-wallet-api.netlify.app/sign-up";
+  const URL = "http://localhost:4000/sign-up";
+  const navigate = useNavigate();
 
+  const body = {
+    email,
+    name,
+    password,
+    confirmPassword,
+  };
 
-    const body = {
-        email,
-        name,
-        password,
-        confirmPassword
+  function signUp(event) {
+    event.preventDefault();
+    setDisabled(true);
+    setLoading(true);
+    signUpSchema();
+  }
+
+  function signUpSchema() {
+    if (password !== confirmPassword) {
+      setDisabled(false);
+      setLoading(false);
+      setErrorText("as senhas são diferentes!");
+      setError(true);
+      return;
     }
-
-    function signUp(event) {
-        event.preventDefault();
-        setDisabled(true);
-        setLoading(true);
-        signUpSchema();
+    if (name.length < 1) {
+      setDisabled(false);
+      setLoading(false);
+      setErrorText("o campo 'nome' deve ser preenchido");
+      setError(true);
+      return;
     }
+    if (email.length < 1) {
+      setDisabled(false);
+      setLoading(false);
+      setErrorText("o campo 'e-mail' deve ser preenchido");
 
-    function signUpSchema() {
-        if (password !== confirmPassword) {
-            setDisabled(false);
-            setLoading(false);
-            setErrorText("as senhas são diferentes!")
-            setError(true);
-            return;
-        }
-        if (name.length < 1) {
-            setDisabled(false);
-            setLoading(false);
-            setErrorText("o campo 'nome' deve ser preenchido")
-            setError(true);
-            return;
-        }
-        if (email.length < 1) {
-            setDisabled(false);
-            setLoading(false);
-            setErrorText("o campo 'e-mail' deve ser preenchido")
-
-            setError(true);
-            return;
-        }
-        if (password.length <= 3) {
-            setDisabled(false);
-            setLoading(false);
-            setErrorText("a senha é curta demais (mín. 4 caracteres)")
-            setError(true);
-            return;
-        }
-        if (password.length > 15) {
-            setDisabled(false);
-            setLoading(false);
-            setErrorText("a senha é grande demais (máx. 15 caracteres")
-            setError(true);
-            return;
-        }
-        else {
-            axios.post(URL, body)
-                .then(() => {
-                    setLoading(false);
-                    navigate("/");
-                })
-                .catch(err => {
-                    setLoading(false);
-                    setDisabled(false);
-                    setErrorText("não foi possível criar sua conta, falha no servidor")
-                    setError(true);
-
-                })
-        }
+      setError(true);
+      return;
     }
-
-    function openModal() {
-        if (error) {
-            return (
-                <ContainerModal onClick={() => setError(false)}>
-                    <h5>Houve um erro: {errorText}</h5>
-                    <h5>Clique em qualquer lugar da caixa para retornar</h5>
-                </ContainerModal>
-            )
-        }
+    if (password.length <= 3) {
+      setDisabled(false);
+      setLoading(false);
+      setErrorText("a senha é curta demais (mín. 4 caracteres)");
+      setError(true);
+      return;
     }
+    if (password.length > 15) {
+      setDisabled(false);
+      setLoading(false);
+      setErrorText("a senha é grande demais (máx. 15 caracteres");
+      setError(true);
+      return;
+    } else {
+      axios
+        .post(URL, body)
+        .then(() => {
+          setLoading(false);
+          navigate("/");
+        })
+        .catch((err) => {
+          setLoading(false);
+          setDisabled(false);
+          setErrorText("não foi possível criar sua conta, falha no servidor");
+          setError(true);
+        });
+    }
+  }
 
-    const openError = openModal();
+  function openModal() {
+    if (error) {
+      return (
+        <ContainerModal onClick={() => setError(false)}>
+          <h5>Houve um erro: {errorText}</h5>
+          <h5>Clique em qualquer lugar da caixa para retornar</h5>
+        </ContainerModal>
+      );
+    }
+  }
 
-    return (
-        <>
-            {error ? openError : null}
-            <Container>
-                <MyWallet>
-                    <h1>MyWallet</h1>
-                </MyWallet>
-                <Form onSubmit={signUp} >
-                    <Input
-                        placeholder="Nome" type="text" autoComplete="off" disabled={disabled} value={name} onChange={e => setName(e.target.value)} />
-                    <Input
-                        placeholder="E-mail" type="email" autoComplete="email" disabled={disabled} value={email} onChange={e => setEmail(e.target.value)} />
-                    <Input
-                        placeholder="Senha (4 a 15 caracteres)" type="password" autoComplete="new-password" disabled={disabled} value={password} onChange={e => setPassword(e.target.value)} />
-                    <Input
-                        placeholder="Confirme a senha" type="password" autoComplete="new-password" disabled={disabled} value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-                    <Button
-                        disabled={disabled}
-                        type="submit" >
-                        {loading ? <ThreeDots color="#fff" height={80} width={80} /> : "Cadastrar"}
-                    </Button>
-                </Form>
-                <Link to={"/"}>
-                    <TextLink>Já tem uma conta? Entre agora!</TextLink>
-                </Link>
-            </Container>
-        </>
-    )
+  const openError = openModal();
 
+  return (
+    <>
+      {error ? openError : null}
+      <Container>
+        <MyWallet>
+          <h1>MyWallet</h1>
+        </MyWallet>
+        <Form onSubmit={signUp}>
+          <Input
+            placeholder='Nome'
+            type='text'
+            autoComplete='off'
+            disabled={disabled}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            placeholder='E-mail'
+            type='email'
+            autoComplete='email'
+            disabled={disabled}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder='Senha (4 a 15 caracteres)'
+            type='password'
+            autoComplete='new-password'
+            disabled={disabled}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            placeholder='Confirme a senha'
+            type='password'
+            autoComplete='new-password'
+            disabled={disabled}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Button
+            disabled={disabled}
+            type='submit'>
+            {loading ? (
+              <ThreeDots
+                color='#fff'
+                height={80}
+                width={80}
+              />
+            ) : (
+              "Cadastrar"
+            )}
+          </Button>
+        </Form>
+        <Link to={"/"}>
+          <TextLink>Já tem uma conta? Entre agora!</TextLink>
+        </Link>
+      </Container>
+    </>
+  );
 }
-
